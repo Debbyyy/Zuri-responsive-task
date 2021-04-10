bc4f5301af82c34e2c26f1eb026571cd4ae3c922
Dhebby <eldhebbyjay609@gmail.com>
2021-04-10 04:26:48 -0700

    i am really struggling with the hamburger icon

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function myFunction() {
-    var x = document.getElementsByClassName("nav-links");
-    if (x.style.display === "block") {
-        x.style.display = "none"
-    }
-    else {
-        x.style.display = "block"
-    }
-}
+// function myFunction() {
+//     var x = document.getElementsByClassName("nav-links");
+//     if (x.style.display === "block") {
+//         x.style.display = "none"
+//     }
+//     else {
+//         x.style.display = "block"
+//     }
+// }
+
+const x = document.getElementsByClassName("nav-links icon").click(function(){
+    var y = document.getElementsByClassName("nav-links").toggleClass("show")
+})
