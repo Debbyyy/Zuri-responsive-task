deaf40f95a5e09d76773c2ad87b710ee2aee97a1
Dhebby <eldhebbyjay609@gmail.com>
2021-04-10 05:42:43 -0700

    styling the hamburger

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // function myFunction() {
-//     var x = document.getElementsByClassName("nav-links");
-//     if (x.style.display === "block") {
-//         x.style.display = "none"
+//     var x = document.getElementById("nav-links");
+//     if (x.style.display === "inline-block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "flex";
 //     }
-//     else {
-//         x.style.display = "block"
-//     }
-// }
+//   }
 
-const x = document.getElementsByClassName("nav-links icon").click(function(){
-    var y = document.getElementsByClassName("nav-links").toggleClass("show")
-})
+// const x = document.getElementsByClassName("nav-links icon").click(function(){
+//     var y = document.getElementsByClassName("nav-links").toggleClass("show")
+// })
+
+// const nav = document.getElementsByClassName("nav-links")
+// const hamburger = document.getElementsByClassName("icon")
+
+function myFunction() {
+    var hamburger = document.getElementsByClassName("nav-links");
+
+    hamburger.classList.toggle("icon")
+}
+
