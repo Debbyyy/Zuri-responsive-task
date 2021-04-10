47b178cdeb610a60ea31b079e6ff8ddc18d5dc24
Dhebby <eldhebbyjay609@gmail.com>
2021-04-10 07:47:07 -0700

    i arranged and cleaned up my code

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-// function myFunction() {
-//     var x = document.getElementById("nav-links");
-//     if (x.style.display === "inline-block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "flex";
-//     }
-//   }
 
-// const x = document.getElementsByClassName("nav-links icon").click(function(){
-//     var y = document.getElementsByClassName("nav-links").toggleClass("show")
-// })
+const hamburger = document.querySelector(".icon")
+const nav = document.querySelector(".nav-links")
 
-// const nav = document.getElementsByClassName("nav-links")
-// const hamburger = document.getElementsByClassName("icon")
-
-function myFunction() {
-    var hamburger = document.getElementsByClassName("nav-links");
-
-    hamburger.classList.toggle("icon")
+const state = {
+    navHidden: true
 }
 
+    const toggleHamburgerbarHandler = () => {
+        if (state.navHidden) {
+hamburger.classList.add("nav-links");
+        }
+        else {
+            hamburger.classList.remove("nav-links")
+        }
+        debz.hamburgerBarhidden = !
+        debz.hamburgerBarhidden;
+    };
+
+
+
+hamburger.addEventListener("click", toggleHamburgerbarHandler);
+icon.addEventListener("click", toggleHamburgerbarHandler);
